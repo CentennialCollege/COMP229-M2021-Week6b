8834e14dbf5c35ec5b8891cb14bce99aac052b76
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-06-14 19:17:52 -0400

    added auth routes

--- Server/Routes/index.ts
+++ Server/Routes/index.ts
@@ -23,4 +23,19 @@ router.get('/services', DisplayServicesPage);
 /* GET contact page. */
 router.get('/contact', DisplayContactPage);
 
+/* GET - display Login page - /login */
+router.get('/login', DisplayLoginPage);
+
+/* GET - display Register page - /register */
+router.get('/register', DisplayRegisterPage);
+
+/* POST - process Login page when user presses Login button */
+router.post('/login', ProcessLoginPage);
+
+/* POST - process Register page when user presses Register button */
+router.post('/register', ProcessRegisterPage);
+
+/* GET - process the Logout page - /logout */
+router.get('/logout', ProcessLogoutPage);
+
 //module.exports = router;
