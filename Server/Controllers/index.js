da518a9857c4773927f26831549b991a74cfc5e0
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-06-14 19:28:29 -0400

    added support for login / logout on main nav

--- Server/Controllers/index.js
+++ Server/Controllers/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DisplayContactPage = exports.DisplayServicesPage = exports.DisplayProjectsPage = exports.DisplayAboutPage = exports.DisplayHomePage = void 0;
+exports.ProcessLogoutPage = exports.ProcessRegisterPage = exports.DisplayRegisterPage = exports.ProcessLoginPage = exports.DisplayLoginPage = exports.DisplayContactPage = exports.DisplayServicesPage = exports.DisplayProjectsPage = exports.DisplayAboutPage = exports.DisplayHomePage = void 0;
 function DisplayHomePage(req, res, next) {
     res.render('index', { title: 'Home', page: 'home' });
 }
@@ -21,4 +21,23 @@ function DisplayContactPage(req, res, next) {
     res.render('index', { title: 'Contact Us', page: 'contact' });
 }
 exports.DisplayContactPage = DisplayContactPage;
+function DisplayLoginPage(req, res, next) {
+    res.render('index', { title: 'Login', page: 'login', messages: req.flash('loginMessage') });
+}
+exports.DisplayLoginPage = DisplayLoginPage;
+function ProcessLoginPage(req, res, next) {
+}
+exports.ProcessLoginPage = ProcessLoginPage;
+function DisplayRegisterPage(req, res, next) {
+    res.render('index', { title: 'Register', page: 'register', messages: req.flash('registerMessage') });
+}
+exports.DisplayRegisterPage = DisplayRegisterPage;
+function ProcessRegisterPage(req, res, next) {
+}
+exports.ProcessRegisterPage = ProcessRegisterPage;
+function ProcessLogoutPage(req, res, next) {
+    req.logout();
+    res.redirect('/login');
+}
+exports.ProcessLogoutPage = ProcessLogoutPage;
 //# sourceMappingURL=index.js.map
